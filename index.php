a47a2298a17bbd95c37101c2ee54df947e6cae0f
Vincent Tjianattan <vincent.tjianattan@binus.ac.id>
2021-02-18 02:33:33 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$link = mysqli_connect($_ENV["DatabaseIPNetwork"],$_ENV["databaseUsername"],$_ENV["databasePassword"],"Trucorp");
+if (!$link){
+echo "tidak bisa konek ke MySQL" . "<br>";
+exit;
+}
+$q = $link->prepare("SELECT * FROM users");
+$q->execute();
+$res = $q->get_result();
+
+while($row=$res->fetch_assoc()){
+	echo var_dump($row)."<br>";
+}
+
+
+mysqli_close($link);
+?>
